0d98fa9e5f4bbb4221eb3d97f30f3d0d484aa36e
Matija Ciganovic <matija.ciganovic@vision.ee.ethz.ch>
2019-01-18 20:19:13 +0100

    params_to_numpy now loops through configs alex,vgg, and stores both exported pickles in ../models

--- lpips-tensorflow/params_to_numpy.py
+++ lpips-tensorflow/params_to_numpy.py
@@ -1,7 +1,10 @@
 # import sys; sys.path += ['models']
 import pickle
 import os
+import sys
+sys.path.insert(0, './PerceptualSimilarity/')
 from models import dist_model as dm
+#OUR CODE
 
 use_gpu = False        # Whether to use GPU
 spatial = False         # Return a spatial map of perceptual distance.
@@ -9,22 +12,28 @@ spatial = False         # Return a spatial map of perceptual distance.
 
 ## Initializing the model
 model = dm.DistModel()
-model_type = 'net-lin'
-model_network = 'alex'
+model_type = 'net'
+model_networks = ['vgg', 'alex'] #choose alex or vgg
 
-print('used network: ' + model_network + ', type: '+model_type)
-# Linearly calibrated models
-model.initialize(model=model_type,net=model_network,use_gpu=use_gpu,spatial=spatial)
+for model_network in model_networks:
+    print('used network: ' + model_network + ', type: '+model_type)
+    # Linearly calibrated models
+    model.initialize(model=model_type,net=model_network,use_gpu=use_gpu,spatial=spatial)
 
 
-# Low-level metrics
-# model.initialize(model='l2',colorspace='Lab')
-# model.initialize(model='ssim',colorspace='RGB')
-print('Model [%s] initialized'%model.name())
+    # Low-level metrics
+    # model.initialize(model='l2',colorspace='Lab')
+    # model.initialize(model='ssim',colorspace='RGB')
+    print('Model [%s] initialized'%model.name())
 
-params_numpy = model.export_params_to_numpy()
-model_path = '/home/cmatija/code/python/DL_project_github/models/'
-model_name = ('alexnet' if model_network=='alex' else model_network) + '_' + model_type
-pickle_out = open(model_path+model_name+'.pickle', "wb+")
-pickle.dump(params_numpy, pickle_out)
-pickle_out.close()
+    params_numpy = model.export_params_to_numpy()
+    model_path = '../models/'
+    print(os.path.dirname(os.path.realpath(__file__)))
+    if not os.path.exists(model_path):
+        print('now creating path')
+        os.makedirs(model_path)
+    model_name = ('alexnet' if model_network=='alex' else model_network) + '_' + model_type
+    pickle_out = open(model_path+model_name+'.pickle', "wb+")
+    print('dumping pickle')
+    pickle.dump(params_numpy, pickle_out)
+    pickle_out.close()
